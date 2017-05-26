72053f9e7a3e5d7ac81e7d85da6dcfe33949c702
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-26 19:20:41 +0100

    trigger

--- test/paint/cli_test.clj
+++ test/paint/cli_test.clj
@@ -2,7 +2,7 @@
   (:require [paint.cli :as cli]
             [clojure.test :as t]))
 
-;; use generative testing also here
+;; TODO: use generative testing also here
 
 (t/deftest parse-command-test
   (t/testing "init command parsed correctly"
