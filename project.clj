bcb84845914a7b326ec9b21b7d3c26b65d295919
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-29 13:37:03 +0100

    update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/AndreaCrotti/paint"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [net.mikera/core.matrix "0.60.3"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/test.check "0.9.0"]]
